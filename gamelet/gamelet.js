1bdb892521f6d6508bf46734ef532f2a8487efeb
odeslf <lipe13silva@hotmail.com>
2023-08-29 17:44:30 -0300

    first commit

---
+++ gamelet/gamelet.js
@@ -0,0 +1,26 @@
+const ball = document.getElementById("ball");
+document.addEventListener("keydown", handleKeyPress);
+let position = 0;
+
+function handleKeyPress(e) {
+  if (e.code === "KeyA") {
+    position = position - 30;
+  }
+  if (e.code === "KeyD") {
+    position = position + 30;
+  }
+  if (e.code === "KeyS") {
+    position = position + 30;
+  }
+  if (e.code === "KeyW") {
+    position = position - 30;
+  }
+  if (position < 0) {
+    position = 0;
+  }
+  refresh(); //update the ball's position
+}
+
+function refresh() {
+  ball.style.top = position + "px";
+}
